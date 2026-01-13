c2fd8e77ed0b272080aa5900edf02aab2bdc9c18
J. Dragon <j.dragon@chivincent.net>
2026-01-14 01:58:48 +0800

    [Chore] Tool Integrations
    Squashed commit of the following:
    
    commit 65f7bede3cd0e62db6489b4f92e091f771204ced
    Author: J. Dragon <j.dragon@chivincent.net>
    Date:   Wed Jan 14 01:58:31 2026 +0800
    
        docs: add readme.md
    
    commit 53204870c2e3dd1ea1378dc1f8ff0e74f2bbef12
    Author: J. Dragon <j.dragon@chivincent.net>
    Date:   Wed Jan 14 01:56:09 2026 +0800
    
        chore: add supabase
    
    commit 80ddf9d0956dff67a414c3b14430726402c07b00
    Author: J. Dragon <j.dragon@chivincent.net>
    Date:   Wed Jan 14 01:39:20 2026 +0800
    
        chore: add tanstack router
    
    commit 240f8b6471853aed7c34ec6562d587b2091bd04f
    Author: J. Dragon <j.dragon@chivincent.net>
    Date:   Wed Jan 14 01:24:58 2026 +0800
    
        chore: add shadcn
    
    commit 374e0f8b933eefa10a6e276a565bd5a6538a1df6
    Author: J. Dragon <j.dragon@chivincent.net>
    Date:   Wed Jan 14 01:16:28 2026 +0800
    
        chore: add biome
    
    commit 9b67e16f4e5e09c9ba99de75133472c2705855a8
    Author: J. Dragon <j.dragon@chivincent.net>
    Date:   Wed Jan 14 01:16:15 2026 +0800
    
        chore: use port 3000

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		tanstackRouter({
+			target: "react",
+			autoCodeSplitting: true,
+		}),
+		react(),
+		tailwindcss(),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+
+	server: {
+		port: 3000,
+	},
+});
