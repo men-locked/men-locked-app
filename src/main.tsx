374e0f8b933eefa10a6e276a565bd5a6538a1df6
J. Dragon <j.dragon@chivincent.net>
2026-01-14 01:16:28 +0800

    chore: add biome

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const el = document.getElementById("root");
+
+el &&
+	createRoot(el).render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	);
